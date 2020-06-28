4b31ee31cc20a1a60008ee305f674b21721bbdb4
Chua Kang Ming <kambing860210@gmail.com>
2020-06-28 20:44:39 +0800

    fix: cors chore: add swagger doc chore: add README.md and rename old one to Requirements.md

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import fs from "fs";
+import jsyaml from "js-yaml";
 import { verbose } from "sqlite3";
+import swaggerUi, { JsonObject } from "swagger-ui-express";
 import app from "./app";
 import buildSchemas from "./schemas";
 
@@ -14,4 +17,8 @@ db.serialize(() => {
   application.listen(port, () =>
     console.log(`App started and listening on port ${port}`),
   );
+
+  const spec = fs.readFileSync("./doc/swagger.yml", "utf8");
+  const oasDoc = jsyaml.safeLoad(spec) as JsonObject;
+  application.use("/api-docs", swaggerUi.serve, swaggerUi.setup(oasDoc));
 });
